5f096976f64e3af239eaa0333d9fd0383e302668
iamgabrielma <gabrielmaldonado.art@gmail.com>
2024-06-23 11:31:36 +0700

    Create Swift Demos xcodeproj

---
+++ Swift Demos/Swift Demos/Swift_DemosApp.swift
@@ -0,0 +1,17 @@
+//
+//  Swift_DemosApp.swift
+//  Swift Demos
+//
+//  Created by Gabriel Maldonado Almendra on 23/6/24.
+//
+
+import SwiftUI
+
+@main
+struct Swift_DemosApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
