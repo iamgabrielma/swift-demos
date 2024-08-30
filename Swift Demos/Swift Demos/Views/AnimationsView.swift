a69675c4076d3c993169cc0005b66043349e5f45
iamgabrielma <gabrielmaldonado.art@gmail.com>
2024-08-30 11:28:47 +0700

    Usage of phaseAnimator

--- Swift Demos/Swift Demos/Views/AnimationsView.swift
+++ Swift Demos/Swift Demos/Views/AnimationsView.swift
@@ -9,17 +9,44 @@ import Foundation
 import SwiftUI
 
 struct AnimationsView: View {
+    var body: some View {
+        VStack {
+            TappableAnimatedRectangleView()
+            BlinkingTextView()
+        }
+    }
+}
+
+struct TappableAnimatedRectangleView: View {
     @State private var selected: Bool = false
 
     var body: some View {
-        Rectangle()
-            .frame(width: 100, height: 100)
-            .scaleEffect(selected ? 2.0 : 1.0)
-            .foregroundColor(selected ? .red : .blue)
-            .onTapGesture {
-                withAnimation {
-                    selected.toggle()
+        ZStack {
+            Rectangle()
+                .frame(width: 100, height: 100)
+                .scaleEffect(selected ? 2.0 : 1.0)
+                .foregroundColor(selected ? .red : .blue)
+                .onTapGesture {
+                    withAnimation {
+                        selected.toggle()
+                    }
                 }
+            Text("Tap me")
+        }
+    }
+}
+
+struct BlinkingTextView: View {
+    var body: some View {
+        Text("This is some blinking text!")
+            .phaseAnimator([false, true]) { content, phase in
+                content
+                    .foregroundStyle(phase ? .red : .primary)
+                    .opacity(phase ? 1.0 : 0.5)
+            } animation: { phase in
+                // By default SwiftUI uses `.spring`, but we can add an animation closure
+                // and change this:
+                    .easeInOut(duration: 1.0)
             }
     }
 }
